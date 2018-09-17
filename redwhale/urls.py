723e8ca132444615e58c6583bb91efc18606bc01
Amitrajit Bose <amitrajitbose9@gmail.com>
2018-09-18 02:41:49 +0530

    Profile image added

--- redwhale/urls.py
+++ redwhale/urls.py
@@ -17,8 +17,11 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls import url
 from redwhale import views
+from django.conf import settings
+from django.conf.urls.static import static
+
 urlpatterns = [
     url(r'^$',views.login_redirect, name='login_redirect'),
     path('admin/', admin.site.urls),
     path('accounts/', include('accounts.urls')),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
